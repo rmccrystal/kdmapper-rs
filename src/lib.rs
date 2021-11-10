f88a02efd99ae8fce9e6d9af4ca41ae81375d389
Ryan McCrystal <rrmccrystal@gmail.com>
2021-11-09 19:00:40 -0800

    Added a safety section

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,9 @@ extern "C" {
 /// If free is set to true, the driver will automatically be unmapped after calling the entry point
 /// If map_mdl is set to true, the driver will be mapped in mdl memory, avoiding being in BigPoolTable (this is recommended as BigPoolTable is detected in some kernel anticheats)
 /// If pass_allocation_ptr is set to true, the driver will be called with the allocation ptr as the first param
+///
+/// # Safety
+/// This function can cause blue screens if given malformed input or the driver causes an exception in the entrypoint, so this function is marked unsafe
 pub unsafe fn kdmapper(driver: &[u8], free: bool, map_mdl: bool, pass_allocation_ptr: bool, param1: u64, param2: u64) -> Option<(i32, i32)> {
     let mut exit = 0;
     let result = map_driver(driver.as_ptr(), free, map_mdl, pass_allocation_ptr, &mut exit, param1, param2);
